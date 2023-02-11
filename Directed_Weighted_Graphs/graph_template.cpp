1dccf8f460503e8ee341fb8d61d43ebb35a81808
kando-123 <100385385+kando-123@users.noreply.github.com>
2023-02-12 00:57:25 +0100

    Some methods implemented.

--- Directed_Weighted_Graphs/graph_template.cpp
+++ Directed_Weighted_Graphs/graph_template.cpp
@@ -3,11 +3,10 @@
 #include <map>
 #include <iostream>
 
-/*
- Implements a directed, weighted graph that enables storing additional data for vertices.
- @param key_t - type of the keys of the vertices, unique in all the graph; ket_t() should not be used
- @param data_t - type of the data assigned to the vertices
- @param weight_t - type of the weights assigned to the edges, should be of a floating-point type
+/** Implements a directed, weighted graph that enables storing additional data for vertices.
+ * @param key_t - type of the keys of the vertices, unique in all the graph; ket_t() should not be used
+ * @param data_t - type of the data assigned to the vertices
+ * @param weight_t - type of the weights assigned to the edges, should be of a floating-point type
  */
 template <class key_t, class data_t, class weight_t = double>
 class my_graph
@@ -33,12 +32,12 @@ class my_graph
 public:
 	void print_graph();
 
-	my_graph() = default;
+	my_graph();
 	
 	void insert_vertex(key_t key, data_t data = data_t());
-	void remove_vertex(key_t key);
+	void erase_vertex(key_t key);
 
-	void reset_key(key_t key, key_t new_key);
+	bool reset_key(key_t key, key_t new_key);
 	void reset_data(key_t key, data_t new_data);
 	data_t& vertex_data(key_t key);
 
@@ -48,13 +47,13 @@ public:
 
 	void insert_edge(key_t tail, key_t head, weight_t weight = 0.0);
 	void insert_undirected_edge(key_t tail, key_t head, weight_t weight = 0.0);
-	void remove_edge(key_t tail, key_t head);
+	void erase_edge(key_t tail, key_t head);
 
 	void reset_weight(key_t tail, key_t head, weight_t weight);
 	weight_t& edge_weight(key_t tail, key_t head);
 };
 
-/* Prints the graph to the console. */
+/// Prints the graph to the console.
 template<class key_t, class data_t, class weight_t>
 void my_graph<key_t, data_t, weight_t>::print_graph()
 {
@@ -69,18 +68,25 @@ void my_graph<key_t, data_t, weight_t>::print_graph()
 	{
 		std::cout << "(" << i.first << ")\t{" << i.second.data << "}\t";
 		if (i.second.outedges.empty())
-			std::cout << "<none>" << std::endl;
+			std::cout << "<none>";
 		else
-		{
 			for (const auto& o : i.second.outedges)
 				std::cout << "[" << o.head << ":" << o.weight << "] ";
-			std::cout << std::endl;
-		}
+		std::cout << std::endl;
 	}
 }
 
-/* Inserts the vertex with key 'key', provided such key is not yet present in the graph,
- and assigns its data with 'data', which is equal to data_t() by default.
+template<class key_t, class data_t, class weight_t>
+my_graph<key_t, data_t, weight_t>::my_graph() : graph_order(0), graph_size(0), incidences()
+{
+	// done :D
+}
+
+/** Inserts vertex 'key', provided such key is not yet present in the graph,
+ * and assigns it with 'data', that is equal to data_t() by default.
+ * @param key_t key - the key that should be inserted
+ * @param data_t data - the data that should be assigned to the vertex
+ * @return true if the replacement succeeded, false otherwise
  */
 template<class key_t, class data_t, class weight_t>
 void my_graph<key_t, data_t, weight_t>::insert_vertex(key_t key, data_t data)
@@ -89,6 +95,77 @@ void my_graph<key_t, data_t, weight_t>::insert_vertex(key_t key, data_t data)
 	++graph_order;
 }
 
+/** Erases vertex 'key' and all edges incident on it.
+ * @param key_t key - the key of the vertex that should be erased
+ */
+template<class key_t, class data_t, class weight_t>
+void my_graph<key_t, data_t, weight_t>::erase_vertex(key_t key)
+{
+	if (incidences.find(key) == incidences.end())
+		return;
+	graph_size -= incidences[key].outedges.size();
+	incidences.erase(key);
+	--graph_order;
+	for (auto& i : incidences)
+	{
+		auto it = i.second.outedges.begin();
+		while (it != i.second.outedges.end())
+		{
+			if (it->head == key)
+			{
+				it = i.second.outedges.erase(it);
+				--graph_size;
+			}
+			else
+				++it;
+		}
+	}
+}
+
+/* Moves the data associated with vertex 'key' to a newly created vertex 'new_key'.
+ * The old vertex is erased; 'key' is replaced by 'new_key' in all the edges.
+ * The replacement is not performed if 'key' is not present or 'new_key' is already present, or 'key' and 'new_key' are equal.
+ * @param key_t key - the key to be replaced
+ * @param key_t new_key - the key to replace with
+ * @return true if the replacement succeeded, false otherwise
+ */
+template<class key_t, class data_t, class weight_t>
+bool my_graph<key_t, data_t, weight_t>::reset_key(key_t key, key_t new_key)
+{
+	if (incidences.find(key) == incidences.end() or incidences.find(new_key) != incidences.end() or key == new_key)
+		return false;
+	incidences[new_key] = std::move(incidences[key]);
+	incidences.erase(key);
+	for (auto& i : incidences)
+	{
+		auto it = i.second.outedges.begin();
+		while (it != i.second.outedges.end())
+		{
+			if (it->head == key)
+				it->head = new_key;
+			++it;
+		}
+	}
+	return true;
+}
+
+/**
+ * 
+ */
+template<class key_t, class data_t, class weight_t>
+void my_graph<key_t, data_t, weight_t>::reset_data(key_t key, data_t new_data)
+{
+	if (incidences.find(key) == incidences.end())
+		return;
+	incidences[key].data = new_data;
+}
+
+/** Inserts an edge from 'tail' to 'head', provided such vertices are present in the graph,
+ * and assigns it with 'weight', that is equal to 0.0 by default.
+ * @param key_t tail - the initial vertex of the edge
+ * @param key_t head - the terminal vertex of the edge
+ * @param weight_t weight - the weight that should be assigned to the edge
+ */
 template<class key_t, class data_t, class weight_t>
 void my_graph<key_t, data_t, weight_t>::insert_edge(key_t tail, key_t head, weight_t weight)
 {
@@ -98,6 +175,12 @@ void my_graph<key_t, data_t, weight_t>::insert_edge(key_t tail, key_t head, weig
 	++graph_size;
 }
 
+ /** Inserts two edges: from 'tail' to 'head' and from 'head' to 'tail',
+  * and assigns them with 'weight', that is equal to 0.0 by default.
+  * @param key_t tail - an endpoint of the edges
+  * @param key_t head - the other endpoint of the edges
+  * @param weight_t weight - the weight that should be assigned to the edges
+  */
 template<class key_t, class data_t, class weight_t>
 void my_graph<key_t, data_t, weight_t>::insert_undirected_edge(key_t tail, key_t head, weight_t weight)
 {
--- Directed_Weighted_Graphs/main.cpp
+++ Directed_Weighted_Graphs/main.cpp
@@ -4,13 +4,41 @@
 int main()
 {
 	my_graph<std::string, unsigned int> towns;
-	towns.print_graph();
-	
 	towns.insert_vertex("Piri", 8'500);
+	towns.insert_vertex("Kafa", 7'400);
+	towns.insert_vertex("Sele", 3'700);
 	towns.insert_vertex("Tumu", 5'000);
+	towns.insert_vertex("Bono", 2'900);
+	towns.insert_vertex("Fiki", 1'200);
+	towns.insert_vertex("Lutu", 1'100);
+	towns.insert_vertex("Govo", 1'200);
+	towns.insert_vertex("Nada", 1'000);
+	towns.insert_vertex("Waya", 500);
+	towns.insert_edge("Piri", "Kafa", 0.5);
+	towns.insert_edge("Kafa", "Piri", 0.4);
+	towns.insert_edge("Piri", "Sele", 1.2);
+	towns.insert_edge("Sele", "Piri", 0.8);
+	//towns.insert_edge("Kafa", "Nada", 0.1);
+	towns.insert_edge("Kafa", "Sele", 2.8);
+	towns.insert_edge("Kafa", "Sele", 1.1);
+	towns.insert_edge("Sele", "Kafa", 0.7);
 	towns.insert_edge("Piri", "Tumu", 1.0);
 	towns.insert_edge("Tumu", "Piri", 1.3);
-	towns.insert_undirected_edge("Piri", "Tumu", 4.5);
+	towns.insert_edge("Piri", "Bono", 0.8);
+	towns.insert_edge("Bono", "Piri", 0.9);
+	towns.insert_edge("Tumu", "Bono", 0.6);
+	towns.insert_edge("Bono", "Tumu", 0.4);
+	towns.insert_edge("Bono", "Fiki", 2.3);
+	towns.insert_edge("Fiki", "Bono", 2.4);
+	towns.insert_edge("Fiki", "Fiki", 1.6);
+	towns.insert_edge("Lutu", "Nada", 0.3);
+	towns.insert_edge("Nada", "Lutu", 0.2);
+	towns.insert_edge("Govo", "Nada", 0.2);
+	towns.insert_edge("Nada", "Govo", 0.1);
+	towns.insert_undirected_edge("Lutu", "Govo", 0.1);
+	towns.print_graph();
+	//towns.erase_vertex("Piri");
+	towns.reset_key("Nada", "New Piri");
 	towns.print_graph();
 	return 0;
 }
